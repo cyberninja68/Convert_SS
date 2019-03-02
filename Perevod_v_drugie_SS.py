50774120b8a48dad6aeb085ad3119568fe7cb152
Cyberninja68 <Ravmm25@gmail.com>
2019-03-02 20:04:27 +0500

    first commit

---
+++ Perevod_v_drugie_SS.py
@@ -0,0 +1,35 @@
+f = open('input.txt','r')
+g = open('output.txt','w')
+i = int(f.readline())
+for j in range(i):
+	a,b = map(int,f.readline().split())
+	def slk (a,b):
+		m = ''
+		s = ''
+		while a != 0:
+			if a%b >= 10:
+				if a%b == 10:
+					m+='A'
+				elif a%b == 11:
+					m+='B'
+				elif a%b == 12:
+					m+='C'
+				elif a%b == 13:
+					m+='D'
+				elif a%b == 14:
+					m+='E'
+				elif a%b == 15:
+					m+='F'
+			else:
+				m+=str(a%b)
+			a//=b
+		m =list(m)
+		m.reverse()
+		for i in range(len(m)):
+			s+=str(m[i])
+		g.write(str(s))
+		g.write('\n')
+		return 
+	slk(a,b)
+g.close()
+f.close()
